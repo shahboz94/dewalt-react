a2dbab0069836a2a3c075db5330e1714332ad199
shokhbozubaydullaev <shahbozubaydullayev3@gmail.com>
2026-01-17 01:47:55 +0900

    fix: retreive data and modify products component

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Box, Button, Container, Stack } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -8,57 +8,70 @@ import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
 import BuildIcon from "@mui/icons-material/Build";
 import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 import ContentCutIcon from "@mui/icons-material/ContentCut";
-
 import SettingsIcon from "@mui/icons-material/Settings";
 import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull";
 import ExtensionIcon from "@mui/icons-material/Extension";
+
 import {
   ProductCategory,
   ProductStatus,
 } from "../../../lib/enums/product.enum";
 
-import { setProducts } from "./slice";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
+
+import { setProducts } from "./slice";
 import { retrieveProducts } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
 
-/** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setProducts: (data: Product[]) => dispatch(setProducts(data)),
 });
+
 const productsRetriever = createSelector(retrieveProducts, (products) => ({
   products,
 }));
 
 export default function Products() {
   const { products } = useSelector(productsRetriever);
+  const { setProducts } = actionDispatch(useDispatch());
 
   const [query, setQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState<
     ProductCategory | "ALL"
   >("ALL");
 
-  const visibleProducts = useMemo(() => {
-    return products.filter((p) => {
-      if (selectedCategory !== "ALL" && p.productCategory !== selectedCategory)
-        return false;
-      if (query && !p.productName.toLowerCase().includes(query.toLowerCase()))
-        return false;
-      return true;
-    });
-  }, [selectedCategory, query]);
-
   const categoryList: (ProductCategory | "ALL")[] = [
     "ALL",
     ...Object.values(ProductCategory),
   ];
+  const [page, setPage] = useState(1);
 
-  const getCategoryIcon = (category: ProductCategory | "ALL") => {
+  useEffect(() => {
+    const productService = new ProductService();
+
+    productService
+      .getProducts({
+        page,
+        limit: 8,
+        order: "createdAt",
+        productCategory:
+          selectedCategory === "ALL" ? undefined : selectedCategory,
+        search: query || undefined,
+      })
+      .then((data) => {
+        setProducts(data); // faqat backenddan kelgan 8 ta
+      })
+      .catch(console.log);
+  }, [page, selectedCategory, query]);
+
+  const getCategoryIcon = (category: ProductCategory) => {
     switch (category) {
       case ProductCategory.DRILL:
         return <BuildIcon />;
@@ -99,15 +112,9 @@ export default function Products() {
 
           <Stack className="dishes-filter-section">
             <Stack className="filter-box">
-              <Button variant="contained" color="primary">
-                NEW
-              </Button>
-              <Button variant="contained" color="secondary">
-                PRICE
-              </Button>
-              <Button variant="contained" color="secondary">
-                VIEWS
-              </Button>
+              <Button variant="outlined">NEW</Button>
+              <Button variant="outlined">PRICE</Button>
+              <Button variant="outlined">VIEWS</Button>
             </Stack>
 
             <Stack direction="row" className="list-category-section">
@@ -118,14 +125,13 @@ export default function Products() {
                     variant={
                       selectedCategory === cat ? "contained" : "outlined"
                     }
-                    color={selectedCategory === cat ? "primary" : "secondary"}
                     onClick={() => setSelectedCategory(cat)}
                     startIcon={
                       cat !== "ALL"
                         ? getCategoryIcon(cat as ProductCategory)
                         : undefined
                     }
-                    sx={{ marginBottom: 1 }}
+                    sx={{ mb: 1 }}
                   >
                     {cat}
                   </Button>
@@ -139,103 +145,74 @@ export default function Products() {
                 flexWrap="wrap"
                 justifyContent="center"
               >
-                {visibleProducts.length !== 0 ? (
-                  visibleProducts.map((product) => {
-                    return (
-                      <Stack key={product._id} className={"product-card"}>
-                        <Stack
-                          className="product-img"
-                          sx={{
-                            backgroundImage: `url(${product.productImages[0]})`,
-                          }}
-                        >
-                          <Button className="shop-btn">
-                            <img
-                              src="/icons/shopping-cart.svg"
-                              alt="Shopping Cart"
-                              style={{ display: "flex" }}
-                            />
-                          </Button>
+                {products.length ? (
+                  products.map((product) => (
+                    <Stack key={product._id} className="product-card">
+                      <Stack
+                        className="product-img"
+                        sx={{
+                          backgroundImage: `url(${
+                            product.productImages?.[0] || "/images/no-image.png"
+                          })`,
+                        }}
+                      >
+                        <Button className="shop-btn">
+                          <img src="/icons/shopping-cart.svg" alt="cart" />
+                        </Button>
 
-                          <Button
-                            className="view-btn"
-                            sx={{
-                              right: "36px",
-                              minWidth: "auto",
-                              padding: "6px",
-                            }}
+                        <Button className="view-btn">
+                          <Badge
+                            badgeContent={
+                              product.productStatus === ProductStatus.ACTIVE
+                                ? product.productViews
+                                : 0
+                            }
                           >
-                            <Badge
-                              badgeContent={
-                                product.productStatus === ProductStatus.ACTIVE
-                                  ? 20
-                                  : 0
-                              }
-                              color="secondary"
-                            >
-                              <RemoveRedEyeIcon sx={{ color: "gray" }} />
-                            </Badge>
-                          </Button>
-                        </Stack>
-
-                        <Box className={"product-desc"}>
-                          <span className={"product-title"}>
-                            {product.productName}
-                          </span>
-                          <div className={"product-desc-icon"}>
-                            <MonetizationOnIcon />
-                            {product.productPrice}
-                          </div>
-                        </Box>
+                            <RemoveRedEyeIcon />
+                          </Badge>
+                        </Button>
                       </Stack>
-                    );
-                  })
+
+                      <Box className="product-desc">
+                        <span className="product-title">
+                          {product.productName}
+                        </span>
+                        <div className="product-desc-icon">
+                          <MonetizationOnIcon />
+                          {product.productPrice}
+                        </div>
+                      </Box>
+                    </Stack>
+                  ))
                 ) : (
-                  <Box className="no-data">Products are not available!</Box>
+                  <Box
+                    className="no-data"
+                    style={{
+                      textAlign: "center",
+                    }}
+                  >
+                    Products are not available!
+                  </Box>
                 )}
               </Stack>
             </Stack>
 
-            <Stack className={"pagination-title"} alignItems={"center"}>
-              <Pagination
-                count={3}
-                renderItem={(item) => (
-                  <PaginationItem
-                    slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-                    {...item}
-                    color={"secondary"}
-                  />
-                )}
-              />
-            </Stack>
+            {/* ===== PAGINATION ===== */}
+            <Pagination
+              count={3}
+              renderItem={(item) => (
+                <PaginationItem
+                  {...item}
+                  slots={{
+                    previous: ArrowBackIcon,
+                    next: ArrowForwardIcon,
+                  }}
+                />
+              )}
+            />
           </Stack>
         </Stack>
       </Container>
-
-      <div className={"brands-logo"}>
-        <Box className="category-title">Our Family Brands</Box>
-        <Stack direction="row" spacing={2} mt={2} justifyContent="center">
-          <Box className="brand-img-1" />
-          <Box className="brand-img-2" />
-          <Box className="brand-img-3" />
-          <Box className="brand-img-4" />
-        </Stack>
-      </div>
-
-      <div className="address">
-        <Container>
-          <Stack className={"address-area"}>
-            <Box className={"add-title"}>Our address</Box>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1498.2231714699258!2d69.22528515394224!3d41.32090667449867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b96d86aa283%3A0x94ce07c99e2fd34e!2sMumtoz%20Restoran!5e0!3m2!1sko!2skr!4v1753959722673!5m2!1sko!2skr"
-              width="1320"
-              height="500"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Restaurant Location"
-            ></iframe>
-          </Stack>
-        </Container>
-      </div>
     </div>
   );
 }
